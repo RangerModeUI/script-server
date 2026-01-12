df5b0a0ce113d2465d018fcf9a862c684f56f64a
RangerModeUI <sergeyali34@yandex.ru>
2026-01-12 21:05:37 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,8 +5,7 @@ sudo apt install ca-certificates gnupg curl
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian 
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update -y
 
